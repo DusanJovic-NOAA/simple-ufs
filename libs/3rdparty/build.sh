fd9679e57e7a22d38dd9af346677a6ac21065598
Dusan Jovic <dusan.jovic@noaa.gov>
2020-01-17 16:18:19 +0000

    Update hdf5 to version 1.10.6

--- libs/3rdparty/build.sh
+++ libs/3rdparty/build.sh
@@ -109,8 +109,8 @@ ZLIB=zlib-1.2.11
 JPEG=jpeg-9c
 JASPER=jasper-1.900.16
 LIBPNG=libpng-1.6.35
-#HDF5=hdf5-1.10.5
-HDF5=hdf5-1.8.21
+HDF5=hdf5-1.10.6
+#HDF5=hdf5-1.8.21
 NETCDF=netcdf-c-4.7.3
 NETCDF_FORTRAN=netcdf-fortran-4.5.2
 ESMF=esmf_8_0_0_src
@@ -120,8 +120,8 @@ WGRIB2=wgrib2-2.0.8
 [ $INSTALL_JPEG           == on ] && download_and_check_md5sum   93c62597eeef81a84d988bccbda1e990   http://www.ijg.org/files/jpegsrc.v9c.tar.gz ${JPEG}.tar.gz
 [ $INSTALL_JASPER         == on ] && download_and_check_md5sum   d0401ced2f5cc7aa1629696c5cba5980   http://www.ece.uvic.ca/~frodo/jasper/software/${JASPER}.tar.gz
 [ $INSTALL_LIBPNG         == on ] && download_and_check_md5sum   d94d9587c421ac42316b6ab8f64f1b85   https://download.sourceforge.net/libpng/${LIBPNG}.tar.gz
-#[ $INSTALL_HDF5           == on ] && download_and_check_md5sum   e115eeb66e944fa7814482415dd21cc4   https://support.hdfgroup.org/ftp/HDF5/releases/${HDF5:0:9}/${HDF5}/src/${HDF5}.tar.gz
-[ $INSTALL_HDF5           == on ] && download_and_check_md5sum   15dbf8b2b466950e1c7be45b66317873   https://support.hdfgroup.org/ftp/HDF5/releases/${HDF5:0:8}/${HDF5}/src/${HDF5}.tar.gz
+[ $INSTALL_HDF5           == on ] && download_and_check_md5sum   37f3089e7487daf0890baf3d3328e54a   https://support.hdfgroup.org/ftp/HDF5/releases/${HDF5:0:9}/${HDF5}/src/${HDF5}.tar.gz
+#[ $INSTALL_HDF5           == on ] && download_and_check_md5sum   15dbf8b2b466950e1c7be45b66317873   https://support.hdfgroup.org/ftp/HDF5/releases/${HDF5:0:8}/${HDF5}/src/${HDF5}.tar.gz
 [ $INSTALL_NETCDF_C       == on ] && download_and_check_md5sum   9e1d7f13c2aef921c854d87037bcbd96   https://www.unidata.ucar.edu/downloads/netcdf/ftp/${NETCDF}.tar.gz
 [ $INSTALL_NETCDF_FORTRAN == on ] && download_and_check_md5sum   864c6a5548b6f1e00579caf3cbbe98cc   https://www.unidata.ucar.edu/downloads/netcdf/ftp/${NETCDF_FORTRAN}.tar.gz
 [ $INSTALL_ESMF           == on ] && download_and_check_md5sum   5cdb3814141068ef15420e7c2d2a158a   http://www.earthsystemmodeling.org/esmf_releases/public/ESMF_8_0_0/${ESMF}.tar.gz
